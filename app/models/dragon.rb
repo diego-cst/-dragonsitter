55aaee3d3940d7fb2bc8e50587315c10535d7808
Matt Pitt <matt.ianpitt@gmail.com>
2018-08-13 17:17:18 +0200

    wrote seed file with users, dragons and bookings

--- app/models/dragon.rb
+++ app/models/dragon.rb
@@ -1,6 +1,8 @@
 class Dragon < ApplicationRecord
-  belongs_to :owner, class_name: "User"
+  belongs_to :owner, class_name: "User", foreign_key: "user_id"
+  has_many :bookings
   has_many :renters, through: :bookings, source: :user
+
   validates :name, presence: true
   validates :price, presence: true
   validates :price, numericality: { only_integer: true }
