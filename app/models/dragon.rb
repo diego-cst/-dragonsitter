a1828f0a861d27a8ccf98f21cb8a1687c08388db
Diego Castellaro <die.cst@gmail.com>
2018-08-13 18:15:19 +0200

    generate dragons controller, index route, index view with basic styling and link to index view on topbar. Add a validation on size, that must be included in 0-4

--- app/models/dragon.rb
+++ app/models/dragon.rb
@@ -6,5 +6,38 @@ class Dragon < ApplicationRecord
   validates :name, presence: true
   validates :price, presence: true
   validates :price, numericality: { only_integer: true }
-  validates :temperament, inclusion: {in: (0..5)}
+  validates :temperament, inclusion: {in: (0..4)}
+  validates :size, inclusion: {in: (0..4)}
+
+  def describe_temperament
+    case self.temperament
+    when 0
+      return "Cuddly"
+    when 1
+      return "Well-mannered"
+    when 2
+      return "Docile"
+    when 3
+      return "Insatible"
+    when 4
+      return "Ferocious"
+    end
+  end
+
+  def describe_size
+    case self.size
+    when 0
+      return "Tiny"
+    when 1
+      return "Small"
+    when 2
+      return "Mid-sized"
+    when 3
+      return "Big"
+    when 4
+      return "Giant"
+    end
+  end
+
+
 end
