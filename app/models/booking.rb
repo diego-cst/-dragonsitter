dbf06cd8840b6e9fe6d90b5a05e76676a2df09f2
Diego Castellaro <die.cst@gmail.com>
2018-08-14 18:13:13 +0200

    added total_price and status_color methods for booking and implemented in profile view

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,21 @@ class Booking < ApplicationRecord
   belongs_to :dragon
   validates :start_date, :end_date, presence: true
   validates :status, inclusion: {in: (0..2)}
+
+  def total_price
+    dragon_price = self.dragon.price
+    duration = self.end_date - self.start_date
+    price = dragon_price * duration
+  end
+
+  def status_color
+    case self.status
+    when 0
+      return "yellow"
+    when 1
+      return "green"
+    when 2
+      return "red"
+    end
+  end
 end
