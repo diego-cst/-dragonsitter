273cf7e5a00a3d56d617347a878e336d599a36f0
Diego Castellaro <die.cst@gmail.com>
2018-08-14 15:33:44 +0200

    create bookings controller and booking new/create routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :dragons, only: [:index, :show, :new, :create]
+  resources :profile, only: [:show]
+  resources :booking, only: [:new, :create]
 
   root to: 'pages#home'
-  resources :profile, only: [:show]
 end
