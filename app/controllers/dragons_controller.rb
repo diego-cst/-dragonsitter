34267fac3aef74a20b8754e6e1cac239c29b22a1
Matt Pitt <matt.ianpitt@gmail.com>
2018-08-14 13:16:38 +0200

    set up dragon new basic

--- app/controllers/dragons_controller.rb
+++ app/controllers/dragons_controller.rb
@@ -1,5 +1,5 @@
 class DragonsController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show]
+  skip_before_action :authenticate_user!, only: [:index, :show, :new]
 
   def index
     @dragons = Dragon.all
@@ -9,4 +9,15 @@ class DragonsController < ApplicationController
     @dragon = Dragon.find(params[:id])
   end
 
+  def new
+    @dragon = Dragon.new(dragon_params)
+  end
+
+  private
+
+  def dragon_params
+    params.require(:dragon).permit(:name, :color, :size, :diet, :temperament, :fire, :location, :price, :description, :photo)
+  end
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :dragons, only: [:index, :show]
+  resources :dragons, only: [:index, :show, :new]
 
   root to: 'pages#home'
   resources :profile, only: [:show]
