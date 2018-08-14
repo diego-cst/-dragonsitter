f6315b27b66a6e15bfec3ed3a943d5d855095467
Louis Masset <lmasset16@gmail.com>
2018-08-14 11:45:31 +0200

    set up basic display for profile dashboard, only verified for unpopulated db

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,6 +1,8 @@
 class ProfileController < ApplicationController
   def show
     @user = current_user
+    @owned_dragons = Dragon.where(owner: @user)
+    @bookings = Dragon.where(renters: @user)
   end
 
 end
