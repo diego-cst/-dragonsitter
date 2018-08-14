ffe19e7202dcbe9b150deffdd2eed95a9b72bd18
Louis Masset <lmasset16@gmail.com>
2018-08-14 12:40:12 +0200

    fix display dragons and messages on profile page

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -2,7 +2,7 @@ class ProfileController < ApplicationController
   def show
     @user = current_user
     @owned_dragons = Dragon.where(owner: @user)
-    @bookings = Dragon.where(renters: @user)
+    @bookings =  @user.bookings
   end
 
 end
