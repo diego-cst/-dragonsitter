2cd82e3ed16b686821aa213e82f7f20b3cf732ab
Matt Pitt <matt.ianpitt@gmail.com>
2018-08-15 16:23:39 +0200

    can geo locate dragons

--- app/controllers/dragons_controller.rb
+++ app/controllers/dragons_controller.rb
@@ -67,7 +67,7 @@ end
 private
 
 def dragon_params
-  params.require(:dragon).permit(:name, :color, :size, :diet, :temperament, :fire, :location, :price, :description, :photo, :photo_cache)
+  params.require(:dragon).permit(:name, :color, :size, :diet, :temperament, :fire, :address, :price, :description, :photo, :photo_cache)
 end
 
 def set_dragon
--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -2,23 +2,6 @@ import GMaps from 'gmaps/gmaps.js';
 import { autocomplete } from '../components/autocomplete';
 
 const mapElement = document.getElementById('map');
-if (mapElement) { // don't try to build a map if there's no div#map to inject in
-  const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
-  const markers = JSON.parse(mapElement.dataset.markers);
-  map.addMarkers(markers);
-  if (markers.length === 0) {
-    map.setZoom(2);
-  } else if (markers.length === 1) {
-    map.setCenter(markers[0].lat, markers[0].lng);
-    map.setZoom(14);
-  } else {
-    map.fitLatLngBounds(markers);
-  }
-}
-
-autocomplete();
-
-
 
 const styles = [
 {
@@ -385,8 +368,25 @@ const styles = [
         ]
     } ];
 
-map.addStyle({
-  styles: styles,
-  mapTypeId: 'map_style'
-});
-map.setStyle('map_style');
+if (mapElement) { // don't try to build a map if there's no div#map to inject in
+  const map = new GMaps({ el: '#map', lat: 0, lng: 0, styles: styles });
+  const markers = JSON.parse(mapElement.dataset.markers);
+  map.addMarkers(markers);
+  if (markers.length === 0) {
+    map.setZoom(2);
+  } else if (markers.length === 1) {
+    map.setCenter(markers[0].lat, markers[0].lng);
+    map.setZoom(6);
+  } else {
+    map.fitLatLngBounds(markers);
+  }
+}
+
+autocomplete();
+
+
+
+
+
+
+
