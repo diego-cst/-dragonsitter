95acbfac7bd06e4590709c4a24b489dae1db16c2
Diego <39303357+diego-cst@users.noreply.github.com>
2018-08-16 15:05:58 +0200

    Update map.js

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -371,7 +371,6 @@ const styles = [
 if (mapElement) { // don't try to build a map if there's no div#map to inject in
   const map = new GMaps({ el: '#map', lat: 0, lng: 0, styles: styles });
   const markers = JSON.parse(mapElement.dataset.markers);
-  console.log(markers);
   map.addMarkers(markers);
 
   // var infowindow = new google.maps.InfoWindow({
